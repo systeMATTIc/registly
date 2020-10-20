e9640d975e650f2386a0d9ef8b0de93b36d8b831
MATTI <mattiopeyemi1003@gmail.com>
2020-10-20 12:20:38 +0100

    Updates

---
+++ precache-manifest.35ea67b24ab89780c81ce98b34ae6d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0199b4edc72a9579b8061b8d7cadc4a2",
+    "url": "/stacksi-fe-test/index.html"
+  },
+  {
+    "revision": "572d91f12deb0df5e49b",
+    "url": "/stacksi-fe-test/static/css/main.5f019f7d.chunk.css"
+  },
+  {
+    "revision": "4ee0d34f3ff5cbdc3dfb",
+    "url": "/stacksi-fe-test/static/js/2.ffaeb96c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/stacksi-fe-test/static/js/2.ffaeb96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572d91f12deb0df5e49b",
+    "url": "/stacksi-fe-test/static/js/main.97b8dff3.chunk.js"
+  },
+  {
+    "revision": "209734fa66566b02e475",
+    "url": "/stacksi-fe-test/static/js/runtime-main.57939680.js"
+  }
+]);
